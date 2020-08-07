579c37189c953dd6337ca32493d0c64eee05efec
David Aronchick <aronchick@gmail.com>
2020-08-07 10:43:58 -0700

    Update create_vm.py

--- create_vm.py
+++ create_vm.py
@@ -1,6 +1,150 @@
-import azure-mgmt-resource
-import azure-mgmt-network
-import azure-mgmt-compute
-import azure-cli-core
+# Import the needed management objects from the libraries. The azure.common library
+# is installed automatically with the other libraries.
+from azure.common.client_factory import get_client_from_cli_profile
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.compute import ComputeManagementClient
+import uuid
 
-print("hello world")
+print(f"Provisioning a virtual machine...some operations might take a minute or two.")
+
+# Step 1: Provision a resource group
+
+# Obtain the management object for resources, using the credentials from the CLI login.
+resource_client = get_client_from_cli_profile(ResourceManagementClient)
+
+# Constants we need in multiple places: the resource group name and the region
+# in which we provision resources. You can change these values however you want.
+RESOURCE_GROUP_NAME = "CI-VM-Sample-rg"
+LOCATION = "centralus"
+
+# Provision the resource group.
+rg_result = resource_client.resource_groups.create_or_update(RESOURCE_GROUP_NAME,
+    {
+        "location": LOCATION
+    }
+)
+
+
+print(f"Provisioned resource group {rg_result.name} in the {rg_result.location} region")
+
+# For details on the previous code, see Example: Provision a resource group
+# at https://docs.microsoft.com/azure/developer/python/azure-sdk-example-resource-group
+
+# Step 2: provision a virtual network
+
+# A virtual machine requires a network interface client (NIC). A NIC requires
+# a virtual network and subnet along with an IP address. Therefore we must provision
+# these downstream components first, then provision the NIC, after which we
+# can provision the VM.
+
+# Network and IP address names
+VNET_NAME = "python-example-vnet"
+SUBNET_NAME = "python-example-subnet"
+IP_NAME = "python-example-ip"
+IP_CONFIG_NAME = "python-example-ip-config"
+NIC_NAME = "python-example-nic"
+
+# Obtain the management object for networks
+network_client = get_client_from_cli_profile(NetworkManagementClient)
+
+# Provision the virtual network and wait for completion
+poller = network_client.virtual_networks.create_or_update(RESOURCE_GROUP_NAME,
+    VNET_NAME,
+    {
+        "location": LOCATION,
+        "address_space": {
+            "address_prefixes": ["10.0.0.0/16"]
+        }
+    }
+)
+
+vnet_result = poller.result()
+
+print(f"Provisioned virtual network {vnet_result.name} with address prefixes {vnet_result.address_space.address_prefixes}")
+
+# Step 3: Provision the subnet and wait for completion
+poller = network_client.subnets.create_or_update(RESOURCE_GROUP_NAME, 
+    VNET_NAME, SUBNET_NAME,
+    { "address_prefix": "10.0.0.0/24" }
+)
+subnet_result = poller.result()
+
+print(f"Provisioned virtual subnet {subnet_result.name} with address prefix {subnet_result.address_prefix}")
+
+# Step 4: Provision an IP address and wait for completion
+poller = network_client.public_ip_addresses.create_or_update(RESOURCE_GROUP_NAME,
+    IP_NAME,
+    {
+        "location": LOCATION,
+        "sku": { "name": "Standard" },
+        "public_ip_allocation_method": "Static",
+        "public_ip_address_version" : "IPV4"
+    }
+)
+
+ip_address_result = poller.result()
+
+print(f"Provisioned public IP address {ip_address_result.name} with address {ip_address_result.ip_address}")
+
+# Step 5: Provision the network interface client
+poller = network_client.network_interfaces.create_or_update(RESOURCE_GROUP_NAME,
+    NIC_NAME, 
+    {
+        "location": LOCATION,
+        "ip_configurations": [ {
+            "name": IP_CONFIG_NAME,
+            "subnet": { "id": subnet_result.id },
+            "public_ip_address": {"id": ip_address_result.id }
+        }]
+    }
+)
+
+nic_result = poller.result()
+
+print(f"Provisioned network interface client {nic_result.name}")
+
+# Step 6: Provision the virtual machine
+
+# Obtain the management object for virtual machines
+compute_client = get_client_from_cli_profile(ComputeManagementClient)
+
+VM_NAME = f"CI-VM-{uuid.uuid4().hex}"
+USERNAME = f"{uuid.uuid4().hex}"
+PASSWORD = f"{uuid.uuid4().hex}"
+
+print(f"Provisioning virtual machine {VM_NAME}; this operation might take a few minutes.")
+
+# Provision the VM specifying only minimal arguments, which defaults to an Ubuntu 18.04 VM
+# on a Standard DS1 v2 plan with a public IP address and a default virtual network/subnet.
+
+poller = compute_client.virtual_machines.create_or_update(RESOURCE_GROUP_NAME, VM_NAME,
+    {
+        "location": LOCATION,
+        "storage_profile": {
+            "image_reference": {
+                "publisher": 'Canonical',
+                "offer": "UbuntuServer",
+                "sku": "16.04.0-LTS",
+                "version": "latest"
+            }
+        },
+        "hardware_profile": {
+            "vm_size": "Standard_DS1_v2"
+        },
+        "os_profile": {
+            "computer_name": VM_NAME,
+            "admin_username": USERNAME,
+            "admin_password": PASSWORD
+        },
+        "network_profile": {
+            "network_interfaces": [{
+                "id": nic_result.id,
+            }]
+        }
+    }
+)
+
+vm_result = poller.result()
+
+print(f"Provisioned virtual machine {vm_result.name}")
