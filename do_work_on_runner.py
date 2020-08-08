59755ae4e6832cbb19e4fdb523d453433668af81
David Aronchick <aronchick@gmail.com>
2020-08-08 02:21:27 +0000

    finally fixing the webhook resubmission

--- do_work_on_runner.py
+++ do_work_on_runner.py
@@ -8,11 +8,14 @@ TAG = sys.argv[2]
 
 print("i'm working really hard on the runner")
 
-url = "https://api.github.com/repos/aronchick/gha-arm-experiment/dispatches"
-return_headers = {
-    "Authorization": "token {}".format(GITHUB_TOKEN),
-    "Accept": "application/vnd.github.everest-preview+json",
-}
-data_body = {'event_type': 'runner_finished', 'client_payload': {'tag': '{}'.format(TAG)}}
+# curl -v POST -H "Accept: application/vnd.github.everest-preview+json"
+# -H "Authorization: token adf0e2f0ecce527264796019dd98c5b8918c9c2f" -H "Content-Type: application/json"
+# --data '{"event_type":"runner_finished"}' https://api.github.com/repos/aronchick/gha-arm-experiment/dispatches
 
-requests.post(url, data=data_body, headers=return_headers)
+url = "https://api.github.com/repos/aronchick/gha-arm-experiment/dispatches"
+return_headers = {"Authorization": f"token {GITHUB_TOKEN}","Accept": "application/vnd.github.everest-preview+json", "Content-Type": "application/json"}
+tag_dict = {"tag": f"{TAG}"}
+data_body = '{"event_type": "runner_finished", "client_payload": {"tag": "'+ TAG +'"} }'
+
+res = requests.post(url, data=data_body, headers=return_headers)
+print(res)
