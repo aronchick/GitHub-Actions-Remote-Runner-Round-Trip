08746d351a5369f3931a358e8b2058b7694013b2
David Aronchick <aronchick@gmail.com>
2020-08-08 19:30:24 +0000

    trying to debug

--- do_work_on_runner.py
+++ do_work_on_runner.py
@@ -12,12 +12,9 @@ if len(sys.argv) >= 2:
 
 print("i'm working really hard on the runner")
 
-# curl -v POST -H "Accept: application/vnd.github.everest-preview+json"
-# -H "Authorization: token adf0e2f0ecce527264796019dd98c5b8918c9c2f" -H "Content-Type: application/json"
-# --data '{"event_type":"runner_finished"}' https://api.github.com/repos/aronchick/gha-arm-experiment/dispatches
-
 url = "https://api.github.com/repos/aronchick/gha-arm-experiment/dispatches"
-return_headers = {"Authorization": f"token {GITHUB_TOKEN}","Accept": "application/vnd.github.everest-preview+json", "Content-Type": "application/json"}
+gh_token = f"token {GITHUB_TOKEN}"
+return_headers = {"Authorization": gh_token,"Accept": "application/vnd.github.everest-preview+json", "Content-Type": "application/json"}
 data_body = '{"event_type": "runner_finished", "client_payload": {"tag": "'+ TAG +'"} }'
 
 res = requests.post(url, data=data_body, headers=return_headers)
